6d39efc5f839c76896cc9c2111b2e0c13c5d9713
darkdarcool <darkdarcool@gmail.com>
2024-06-01 22:48:06 -0400

    inline lua trimming and formatting

--- hosts/darkdarcool/nvim.nix
+++ hosts/darkdarcool/nvim.nix
@@ -26,95 +26,14 @@
     globals.mapleader = " ";
 
     extraConfigLua = '' 
-	    -- require("inlay-hints").setup()
-            vim.api.nvim_create_autocmd("QuitPre", {
-      	callback = function()
-              local tree_wins = {}
-              local floating_wins = {}
-              local wins = vim.api.nvim_list_wins()
-              for _, w in ipairs(wins) do
-      	  local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
-                if bufname:match("NvimTree_") ~= nil then
-      	    table.insert(tree_wins, w)
-                end
-                if vim.api.nvim_win_get_config(w).relative ~= "" then
-      	    table.insert(floating_wins, w)
-                end
-              end
-              if 1 == #wins - #floating_wins - #tree_wins then
-      	  -- Should quit, so we close all invalid windows.
-                for _, w in ipairs(tree_wins) do
-      	    vim.api.nvim_win_close(w, true)
-      	  end
-              end
-            end
-            })
-            
+      -- get rid of the sign column (it moves text and thats a no no)
+      vim.opt.signcolumn = "no"
       
-            vim.opt.signcolumn = "no"
-	    --[[
-            require('diagflow').setup({
-      	enable = true,
-      	max_width = 60,  -- The maximum width of the diagnostic messages
-      	max_height = 10, -- the maximum height per diagnostics
-      	  severity_colors = {  -- The highlight groups to use for each diagnostic severity level
-      	    error = "DiagnosticFloatingError",
-      	    warning = "DiagnosticFloatingWarn",
-      	    info = "DiagnosticFloatingInfo",
-      	    hint = "DiagnosticFloatingHint",
-      	},
-      	format = function(diagnostic)
-      	  return diagnostic.message
-      	end,
-      	gap_size = 1,
-      	scope = 'line', -- 'cursor', 'line' this changes the scope, so instead of showing errors under the cursor, it shows errors on the entire line.
-      	padding_top = 0,
-      	padding_right = 0,
-      	text_align = 'right', -- 'left', 'right'
-      	placement = 'top', -- 'top', 'inline'
-      	inline_padding_left = 0, -- the padding left when the placement is inline
-      	update_event = { 'DiagnosticChanged', 'BufReadPost' }, -- the event that updates the diagnostics cache
-      	toggle_event = { }, -- if InsertEnter, can toggle the diagnostics on inserts
-      	show_sign = false, -- set to true if you want to render the diagnostic sign before the diagnostic message
-      	render_event = { 'DiagnosticChanged', 'CursorMoved' },
-      	border_chars = {
-      	  top_left = "╭",
-      	  top_right = "╮",
-      	  bottom_left = "╰",
-      	  bottom_right = "╯",
-      	  horizontal = "─",
-      	  vertical = "│"
-      	},
-      	show_borders = true,
-            })
-	    --]]
-
-	  local hover = function(_, result, ctx, config) 
-	    
-	  end
-	    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "rounded",
-  underline = true
-})
-
-vim.cmd("hi FloatBorder guibg=NONE guifg=#F2E2C3")
-		require("pretty_hover").setup({})
---[[
-		vim.api.nvim_create_autocmd('LspAttach', {
-		  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-		#  callback = function(ev)
-		#    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		#    if client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-		#      vim.lsp.inlay_hint.enable(ev.buf, true)
-		#    end
-		#  end,
-		#  }
-		#)
-		--]]
-
-
-
-
+      -- rounded hover doc (its 10x better)
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = "rounded",
+	underline = true
+      })
     '';
 
     plugins = {
